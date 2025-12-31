bf896ce9553af57121c6d2c8299d98a9cf82ee3e
nourhene <benothmennourhen8@gmail.com>
2025-12-31 16:48:56 +0100

    feat(controllers): add base controllers for users

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+export const login=async (req: Request, res: Response):Promise<void>=>{
+
+}
+export const register=async(req:Request,res:Response):Promise<void>=>{
+
+}
+export const getUsers = async (req: Request, res: Response):Promise<void> => {
+ 
+};
+
+export const getUserById = async (req: Request, res: Response):Promise<void> => {
+ 
+};
